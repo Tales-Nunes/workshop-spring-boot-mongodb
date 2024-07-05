8fb4f23f22a698d523c0b094d2e2fbd30bbd1db4
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-07-05 19:54:50 -0300

    Acrescentando comentários aos posts e correção na classe Post

--- src/main/java/com/talesnunes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/talesnunes/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.talesnunes.workshopmongo.config;
 import com.talesnunes.workshopmongo.domain.Post;
 import com.talesnunes.workshopmongo.domain.User;
 import com.talesnunes.workshopmongo.dto.AuthorDTO;
+import com.talesnunes.workshopmongo.dto.CommentDTO;
 import com.talesnunes.workshopmongo.repository.PostRepository;
 import com.talesnunes.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,9 +37,18 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
+
+
     }
 }
