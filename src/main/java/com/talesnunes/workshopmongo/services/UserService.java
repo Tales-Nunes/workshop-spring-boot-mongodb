1e61e2e9ef08d7d0f08f3b4f4be62732e6117cda
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-07-05 16:08:26 -0300

    Obtendo um usuário por id

--- src/main/java/com/talesnunes/workshopmongo/services/UserService.java
+++ src/main/java/com/talesnunes/workshopmongo/services/UserService.java
@@ -2,11 +2,13 @@ package com.talesnunes.workshopmongo.services;
 
 import com.talesnunes.workshopmongo.domain.User;
 import com.talesnunes.workshopmongo.repository.UserRepository;
+import com.talesnunes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
