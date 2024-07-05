cdea7c58129727fbe926d2b5b5386ec84bc469fe
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-07-05 20:19:39 -0300

    Consulta simples com query methods

--- src/main/java/com/talesnunes/workshopmongo/resources/PostResource.java
+++ src/main/java/com/talesnunes/workshopmongo/resources/PostResource.java
@@ -1,18 +1,16 @@
 package com.talesnunes.workshopmongo.resources;
 
 import com.talesnunes.workshopmongo.domain.Post;
-import com.talesnunes.workshopmongo.domain.User;
-import com.talesnunes.workshopmongo.dto.UserDTO;
+import com.talesnunes.workshopmongo.resources.util.URL;
 import com.talesnunes.workshopmongo.services.PostService;
-import com.talesnunes.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+
 import java.util.List;
-import java.util.stream.Collectors;
+
+
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -25,6 +23,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value= "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 
