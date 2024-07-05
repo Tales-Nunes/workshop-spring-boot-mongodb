369f917e8fe08bd166edc15af58c604599a8d9a7
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-07-05 09:52:43 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/talesnunes/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.talesnunes.workshopmongo.services;
+
+import com.talesnunes.workshopmongo.domain.User;
+import com.talesnunes.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
