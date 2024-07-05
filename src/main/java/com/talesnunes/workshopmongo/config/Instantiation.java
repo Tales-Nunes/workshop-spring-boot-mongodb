ad53354fc0a4e3fdef7f39b40da6d5d5d8daacf0
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-07-05 17:20:06 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/talesnunes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/talesnunes/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.talesnunes.workshopmongo.config;
 
 import com.talesnunes.workshopmongo.domain.Post;
 import com.talesnunes.workshopmongo.domain.User;
+import com.talesnunes.workshopmongo.dto.AuthorDTO;
 import com.talesnunes.workshopmongo.repository.PostRepository;
 import com.talesnunes.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +33,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
